17d907f7806aecb67859c69816d7d60197cbe6da
Hector <adeniyi.olaitanhector@yahoo.com>
2023-12-27 11:49:20 +0100

    fixed min height

--- src/app/layouts/CarouselSection.jsx
+++ src/app/layouts/CarouselSection.jsx
@@ -10,42 +10,41 @@ const CarouselSection = () => {
     <CarouselCont id='recents'>
         <h1 data-aos='fade-down-right'>Our Recent Works</h1>
         <Carousel showThumbs={false} showArrows={true} showIndicators={true} infiniteLoop={true} autoPlay={true} transitionTime={1000} centerMode={true}  >
-                <div>
-                    <img src='/images/flex2.jpg' /* width={500} height={281.25} */ alt='' />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src="/images/flex2.jpg" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="/images/flex2.jpg" />
-                    <p className="legend">Legend 3</p>
-                </div>
-                <div>
-                    <img src="/images/flex2.jpg" />
-                    <p className="legend">Legend 4</p>
-                </div>
-                <div>
-                    <img src="/images/flex2.jpg" />
-                    <p className="legend">Legend 5</p>
-                </div>
-                <div>
-                    <img src="/images/flex2.jpg" />
-                    <p className="legend">Legend 6</p>
-                </div>
-            </Carousel>
-        </CarouselCont>
+            <div>
+                <img src='/images/flex2.jpg' /* width={500} height={281.25} */ alt='' />
+                <p className="legend">Legend 1</p>
+            </div>
+            <div>
+                <img src="/images/flex2.jpg" />
+                <p className="legend">Legend 2</p>
+            </div>
+            <div>
+                <img src="/images/flex2.jpg" />
+                <p className="legend">Legend 3</p>
+            </div>
+            <div>
+                <img src="/images/flex2.jpg" />
+                <p className="legend">Legend 4</p>
+            </div>
+            <div>
+                <img src="/images/flex2.jpg" />
+                <p className="legend">Legend 5</p>
+            </div>
+            <div>
+                <img src="/images/flex2.jpg" />
+                <p className="legend">Legend 6</p>
+            </div>
+        </Carousel>
+    </CarouselCont>
   )
 }
 
 export default CarouselSection
 
 const CarouselCont = styled.div`
+    min-height: 100vh;
     h1 {
         text-align: center;
         margin: 2rem 0;
     }
-
-    min-height: 300px;
 `
